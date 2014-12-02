95e1404567f255d8e6da98382073d30ac13b7b65
Erin Sparling <erin@everyplace.net>
2014-12-01 20:14:57 -0500

    Added correct reference to library.

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php 
 
-	require_once("YouTube-CLosed-Captions/closed-captions.php");
+	require_once("lib/YouTube-CLosed-Captions/closed-captions.php");
 	header('Content-Type: application/json');
-	$captions = getClosedCaptionsForVideo($_GET['url']);
-	echo json_encode($captions);
+	if($_GET['url']) {
+		$captions = getClosedCaptionsForVideo($_GET['url']);
+		echo json_encode($captions);	
+	} else {
+		echo "Please append ?url=<a youtube video id> to the end of this url";
+	}
+	
